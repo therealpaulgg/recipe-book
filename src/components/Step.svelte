9c10367430d1801b16e722adb926e2437bbb0097
therealpaulgg <paul@paul.systems>
2022-07-04 20:16:15 -0700

    more reasonable checkbox size

--- src/components/Step.svelte
+++ src/components/Step.svelte
@@ -4,8 +4,8 @@
     const id = uniqueid();
 </script>
 
-<div class="text-lg mb-2">
-    <input class="form-checkbox rounded w-6 h-6 mr-1" type="checkbox" {id} />
+<div class="text-lg mb-2 flex items-center gap-2">
+    <input class="form-checkbox rounded w-4 h-4" type="checkbox" {id} />
     {#if typeof name === "string"}
         <label for={id} class="cursor-pointer">{name}</label>
     {/if}
