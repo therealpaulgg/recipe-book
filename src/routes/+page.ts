dda33db73c3be8639868ffce33a8d49bec095e68
therealpaulgg <paul@paul.systems>
2023-01-03 21:49:54 -0700

    index.json fix

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
 import { base } from "$app/paths";
 export async function load({ fetch }) {
-    const categories = await fetch(`${base}/.json`).then((r) => r.json());
+    const categories = await fetch(`${base}/categories.json`).then((r) => r.json());
     return { categories };
 }
