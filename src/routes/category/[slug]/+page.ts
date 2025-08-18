edf47bf6b716849d3aae8b042921b037292102e2
Paul Gellai <paul@paul.systems>
2025-08-17 17:47:37 -0700

    alphabetical order sorting

--- src/routes/category/[slug]/+page.ts
+++ src/routes/category/[slug]/+page.ts
@@ -3,5 +3,6 @@ import type { Recipe } from "src/models/RecipeMetadata"
 export async function load({ fetch, params }) {
     const slug = params.slug;
     const recipes: Recipe[] = await fetch(`${base}/category/${slug}.json`).then((r) => r.json());
+    recipes.sort((a, b) => a.metadata.title.localeCompare(b.metadata.title))
     return { recipes, slug };
 }
